64e02d7b1a5aee4fafc87acab4e2bd6bc9c73d61
Muhammad Fajar <mfjr48@gmail.com>
2025-07-18 06:29:43 +0700

    add: auto update css

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>60th PGN Flipbook</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css?<?= time(); ?>">
 </head>
 
 <body>
